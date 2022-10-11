a1cf2adadd0a41e73dcd34545786da0e48d9f1b5
LeilaJM <leilajmasset@gmail.com>
2022-10-11 15:15:04 -0300

    Initial Commit

---
+++ TesteGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TesteGit
+//
+//  Created by Leila Masset on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TesteGit/TesteGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteGitApp.swift
+//  TesteGit
+//
+//  Created by Leila Masset on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TesteGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
